ff2b5fff6afc45d5eaefa50fa104033f943c70b0
Lucas Fonseca <lucasfreireri@gmail.com>
2021-09-19 09:45:56 -0300

    Added page for user to make new entries about each topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The home page for Learning Log."""
@@ -23,7 +23,7 @@ def topic(request, topic_id):
 def new_topic(request):
     """Add a new topíc."""
     if request.method != 'POST':
-        # No data submitted; create a black form.
+        # No data submitted; create a blank form.
         form = TopicForm()
     else:
         # POST data submitted; process data.
@@ -35,3 +35,26 @@ def new_topic(request):
     # Display a black or invalid form. 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry to a specific topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    
+    # Display a blank or invalid form. 
+    context = {
+        'form': form,
+        'topic': topic}
+        
+    return render(request, 'learning_logs/new_entry.html', context)
